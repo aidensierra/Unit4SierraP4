4035a6b95c8fb9003f158fa6ed8d679fb2b45d11
aidensierra <768351@student.dusd.net>
2026-04-28 11:46:31 -0700

    Added things
    Added things

--- P4SierraUnit4/Assets/Scripts/SpawnManager.cs
+++ P4SierraUnit4/Assets/Scripts/SpawnManager.cs
@@ -3,11 +3,28 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
+    public bool hasPowerup;
+
+    private float spawnRange = 9;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Instantiate(enemyPrefab, new Vector3(0, 0, 6), enemyPrefab.transform.rotation);
+        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+    }
+    private Vector3 GenerateSpawnPosition()
+    {
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);
+        float spawnPosZ = Random.Range(-spawnRange, spawnRange);
+        
+        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
+
+        return randomPos;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Powerup"));
     }
 
     // Update is called once per frame
